90d6f4460e59920218bf12e2ef1d45a7beee8d87
andrew stebenne <arthropodSeven@protonmail.com>
2020-11-01 17:23:56 -0500

    config postcss: json -> js, replace postcss-preset-env with precss

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  plugins: [
+    'postcss-flexbugs-fixes',
+    [
+      'precss',
+      {
+        'postcss-preset-env': {
+          autoprefixer: {
+            flexbox: 'no-2009'
+          },
+          stage: 1
+        }
+      }
+    ]
+  ]
+}
