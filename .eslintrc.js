2b46757474b1f3f73e5a9e40ae1b292770345d69
andrew stebenne <arthropodSeven@protonmail.com>
2020-11-14 21:51:03 -0500

    prettier on eslintrc. lol

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,18 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
+    node: true
   },
-  extends: ["plugin:react/recommended", "standard"],
+  extends: ['plugin:react/recommended', 'standard'],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module'
   },
-  plugins: ["react"],
+  plugins: ['react'],
   rules: {
-    "react/react-in-jsx-scope": 0,
-  },
-};
+    'react/react-in-jsx-scope': 0
+  }
+}
