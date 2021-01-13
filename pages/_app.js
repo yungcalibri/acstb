3676c0136f15f9d4d8f3b8cfc773b1582b6ea82b
andrew stebenne <arthropodSeven@protonmail.com>
2021-01-13 00:11:15 -0500

    add a reference footer with color names

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,31 @@
+import { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import '../styles/index.css'
 
 function MyApp ({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Component {...pageProps} />
+
+      <footer data-role="reference">
+        <div data-bg="peat" data-tc="nowhere">
+          peat
+        </div>
+        <div data-bg="moss" data-tc="nowhere">
+          moss
+        </div>
+        <div data-bg="twilight" data-tc="nowhere">
+          twilight
+        </div>
+        <div data-bg="stream" data-tc="nowhere">
+          stream
+        </div>
+        <div data-bg="nowhere" data-tc="moss">
+          nowhere
+        </div>
+      </footer>
+    </Fragment>
+  )
 }
 
 export default MyApp
