8f757b12150f3ce015f4d0fd1a58538c0b9a8e0c
andrew stebenne <arthropodSeven@protonmail.com>
2021-01-12 22:44:31 -0500

    baseline

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,13 @@ const Home = () => (
         <h2>A Website by Andrew Stebenné</h2>
       </div>
     </header>
+
+    <main>
+      <h1>Available Here</h1>
+      <h2>Blog Posts</h2>
+      <h2>Projects</h2>
+      <h2>COntact Me</h2>
+    </main>
   </article>
 )
 
