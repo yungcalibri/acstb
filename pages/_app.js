b8acf63ad779ad4dee4a9b4f9cf2774b5feb225c
andrew stebenne <arthropodSeven@protonmail.com>
2021-03-04 12:43:54 -0500

    remove the old reference footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,7 @@
-import { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import 'styles/index.css'
 
-function MyApp ({ Component, pageProps }) {
-  return (
-    <Fragment>
-      <Component {...pageProps} />
-
-      <footer data-role="reference">
-        <div data-bg="peat" data-tc="nowhere">
-          peat
-        </div>
-        <div data-bg="moss" data-tc="nowhere">
-          moss
-        </div>
-        <div data-bg="twilight" data-tc="nowhere">
-          twilight
-        </div>
-        <div data-bg="stream" data-tc="nowhere">
-          stream
-        </div>
-        <div data-bg="nowhere" data-tc="moss">
-          nowhere
-        </div>
-      </footer>
-    </Fragment>
-  )
-}
+const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />
 
 export default MyApp
 
