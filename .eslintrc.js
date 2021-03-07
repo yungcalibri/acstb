a316bdff07711a61725e2ab42f47bed0723ef84d
andrew stebenne <arthropodSeven@protonmail.com>
2021-03-07 16:19:37 -0500

    turn off proptypes validation

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
   plugins: ['react'],
   rules: {
     'react/react-in-jsx-scope': 0,
-    'react/no-unescaped-entities': 0
+    'react/no-unescaped-entities': 0,
+    'react/prop-types': 0
   }
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,5 @@
-import PropTypes from 'prop-types'
 import 'styles/index.css'
 
 const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />
 
 export default MyApp
-
-MyApp.propTypes = {
-  Component: PropTypes.Component,
-  pageProps: PropTypes.Object
-}
