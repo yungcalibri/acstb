eadf6f6c4d8814ae8bf37ba628cde43cb20b4fea
andrew stebenne <arthropodSeven@protonmail.com>
2021-03-08 00:58:06 -0500

    use global Header

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
+import React from 'react'
+import Header from 'components/Header'
 import 'styles/index.css'
 
-const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }) => (
+  <React.Fragment>
+    <Header />
+    <Component {...pageProps} />
+  </React.Fragment>
+)
 
 export default MyApp
