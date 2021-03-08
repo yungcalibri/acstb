af57925a5f2eeddb9fe1529dbb5a334210bd48ea
andrew stebenne <arthropodSeven@protonmail.com>
2021-03-08 18:14:28 -0500

    lint

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,25 +1,19 @@
-import { useRouter } from 'next/router'
 import Link from 'next/link'
 import { PageCenter } from 'components/Center'
 
-const Header = (props) => {
-  const { pathname } = useRouter()
-  const atRoot = /^\/$/.test(pathname)
-
-  return (
-    <header>
-      <PageCenter>
-        <h1>
-          <Link href="/">
-            <code>
-              <a>acstb.name</a>
-            </code>
-          </Link>
-        </h1>
-        {/* Should swap out the self-aggrandizement for some breadcrumbs here when the time comes. */}
-      </PageCenter>
-    </header>
-  )
-}
+const Header = () => (
+  <header>
+    <PageCenter>
+      <h1>
+        <Link href="/">
+          <code>
+            <a>acstb.name</a>
+          </code>
+        </Link>
+      </h1>
+      {/* Should swap out the self-aggrandizement for some breadcrumbs here when the time comes. */}
+    </PageCenter>
+  </header>
+)
 
 export default Header
