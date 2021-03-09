4d727fceec758fc14dfd51c0b988054e53a040d6
andrew stebenne <arthropodSeven@protonmail.com>
2021-03-09 16:24:00 -0500

    reintroduce lint errors

--- components/Box/index.js
+++ components/Box/index.js
@@ -2,17 +2,20 @@ import { propStyles } from "util/style";
 import styles from "./box.module.css";
 
 const Box = (props) => {
-  const { borderWidth, children, className = "", padding, ...rest } = props;
+  const { borderWidth,
+    children,
+    className = "", padding, ...rest } = props;
 
   const myClass = `${styles.box} ${className}`;
 
   const myStyles = propStyles(
-    [borderWidth, "--border-width"],
-    [padding, "padding"]
+    [borderWidth, '--border-width'],
+    [padding, 'padding']
   );
 
   return (
-    <div className={myClass} {...rest}>
+    <div className={myClass} {...rest}
+    >
       {children}
       <style jsx>{`
         .${styles.box} {
