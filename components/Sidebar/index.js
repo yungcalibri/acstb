356fc47ae8b754df67ee656b4c42640443687ec1
andrew stebenne <arthropodSeven@protonmail.com>
2021-03-13 15:58:36 -0500

    add sidebar
    the vast majority of the logic here goes to determining the exact values that should be written into the CSS. It might be good to construct another kind of component, if many more are like this.

--- components/Sidebar/index.js
+++ components/Sidebar/index.js
@@ -1,18 +1,57 @@
 import styles from "./sidebar.module.css";
 
+const me = styles.sidebarRoot;
+const LEFT = "left";
+const FIRST_CHILD = ":first-child";
+const LAST_CHILD = ":last-child";
+
 const Sidebar = (props) => {
   const {
     children,
-    _side,
-    _sideWidth,
-    _contentMin,
-    _space,
-    _noStretch,
+    side = LEFT,
+    sideWidth,
+    contentMin = "50%",
+    space: rawSpace = "var(--s1)",
+    noStretch = false,
     ...rest
   } = props;
+
+  const sidebarSelector = side === LEFT ? FIRST_CHILD : LAST_CHILD;
+
+  const space = String(rawSpace) === "0" ? "0px" : rawSpace;
+
   return (
-    <div className={styles.sidebar} {...rest}>
+    <div className={styles.sidebarRoot} {...rest}>
       {children}
+      {/* My styles */}
+      <style jsx>{`
+        ${me} {
+          ${sideWidth ? `flex-basis: ${sideWidth}` : ""}
+          --space: ${space};
+        }
+      `}</style>
+      {/* Intermediary wrapper styles */}
+      <style jsx>{`
+        ${me} > * {
+          margin: calc(${space} / 2 * -1);
+          ${noStretch ? "align-items: flex-start;" : ""}
+        }
+      `}</style>
+      {/* Second child styles */}
+      <style jsx>{`
+        ${me} > * > * {
+          margin: calc(${space} / 2);
+          ${this.sideWidth ? `flex-basis: ${this.sideWidth};` : ""}
+        }
+      `}</style>
+      {/* Styles for whichever element is the sidebar */}
+      <style jsx>{`
+        ${me} > * > ${sidebarSelector} {
+          flex-basis: 0;
+          flex-grow: 999;
+          min-width: calc(${contentMin} - ${space});
+        }
+      `}</style>
     </div>
   );
 };
