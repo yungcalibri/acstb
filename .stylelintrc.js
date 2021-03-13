f9562311989885eb5c9a9785061b36d0bae6c46c
andrew stebenne <arthropodSeven@protonmail.com>
2021-03-12 19:32:48 -0500

    update stylelint config to be more useful

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,11 +1,15 @@
 module.exports = {
   plugins: ["stylelint-high-performance-animation"],
   extends: [
-    "stylelint-config-idiomatic-css",
+    "stylelint-config-standard",
     "stylelint-a11y/recommended",
     "stylelint-config-prettier",
   ],
   rules: {
     "plugin/no-low-performance-animation-properties": true,
+    "value-keyword-case": undefined,
+    "no-duplicate-selectors": undefined,
+    "custom-property-empty-line-before": undefined,
+    "declaration-empty-line-before": undefined,
   },
 };
