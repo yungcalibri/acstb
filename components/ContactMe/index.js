8c451c09801ed2d99867c51ff775ffe7236a9ad6
andrew stebenne <arthropodSeven@protonmail.com>
2021-03-16 22:51:45 -0400

    update ContactMe

--- components/ContactMe/index.js
+++ components/ContactMe/index.js
@@ -1,30 +1,55 @@
 import Box from "components/Box";
 import Stack from "components/Stack";
+import Cluster from "components/Cluster";
 import styles from "./contactMe.module.css";
 
 const ContactMe = (props) => {
-  const { className = "", relMe = false, ...rest } = props;
+  const { className = "", ...rest } = props;
   const myClass = `${styles.contactMe} ${className}`;
 
   return (
-    <Box className={myClass} borderWidth="var(--s-4)" {...rest}>
-      <Stack asList>
-        <div>
-          <a
-            href="https://github.com/arthropodSeven/"
-            rel={`${relMe ? "me" : ""} external`}>
-            github.com/arthropodSeven
-          </a>
-        </div>
-        <div>
-          <a
-            href="mailto:arthropodSeven@pm.me"
-            rel={`${relMe ? "me" : ""} external`}>
-            arthropodSeven@pm.me
-          </a>
-        </div>
-      </Stack>
-    </Box>
+    <Stack className={myClass} asList space="var(--s-2)" {...rest}>
+      <div>
+        <ContactCard
+          href="https://github.com/arthropodSeven/"
+          className="padding:0 margin:0">
+          <div>github.com/arthropodSeven</div>
+          <div
+            style={{
+              height: "50px",
+              width: "50px",
+              background: "var(--color-background)",
+            }}></div>
+        </ContactCard>
+      </div>
+      <div>
+        <ContactCard
+          href="mailto:arthropodSeven@pm.me"
+          className="padding:0 margin:0">
+          <div>arthropodSeven@pm.me</div>
+          <div
+            style={{
+              height: "50px",
+              width: "50px",
+              background: "var(--color-background)",
+            }}>
+            <span />
+          </div>
+        </ContactCard>
+      </div>
+    </Stack>
+  );
+};
+
+const ContactCard = (props) => {
+  const { children, className = "", href, ...rest } = props;
+
+  return (
+    <a href={href} className={className} rel="me external" {...rest}>
+      <Box className={styles.contactCard} borderWidth="var(--s-3)">
+        <Cluster align="center">{children}</Cluster>
+      </Box>
+    </a>
   );
 };
 
