bcaa3e51749a989d3cf3f79a52782a1aacdef475
andrew stebenne <arthropodSeven@protonmail.com>
2021-03-16 22:16:15 -0400

    add v0 Switcher layout

--- components/Switcher/index.js
+++ components/Switcher/index.js
@@ -1,6 +1,36 @@
+import { propStyles } from "util/style";
+import styles from "./switcher.module.css";
+
 const Switcher = (props) => {
-  const { children, ...rest } = props;
-  return <div {...rest}>{children}</div>;
+  const {
+    children,
+    className = "",
+    threshold,
+    space,
+    limit = 7,
+    ...rest
+  } = props;
+
+  const myClass = [styles.switcher, className].join(" ");
+
+  return (
+    <div className={myClass} {...rest}>
+      <div>{children}</div>
+      <style jsx>{`
+        .${styles.switcher} {
+          ${propStyles([threshold, "--threshold"], [space, "--space"])}
+        }
+      `}</style>
+      <style jsx>{`
+        :global(.${styles.switcher} > * > :nth-last-child(n + ${limit})) {
+          ${propStyles([limit, "flex-basis: 100%"])}
+        }
+        :global(.${styles.switcher} > * > :nth-last-child(n + ${limit}) ~ *) {
+          ${propStyles([limit, "flex-basis: 100%"])}
+        }
+      `}</style>
+    </div>
+  );
 };
 
 export default Switcher;
