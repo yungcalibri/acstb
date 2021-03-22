f8a62de4533a6080cf09ad962457150b8da2e912
andrew stebenne <arthropodSeven@protonmail.com>
2021-03-22 18:40:32 -0400

    use global styles for Cluster

--- components/Cluster/index.js
+++ components/Cluster/index.js
@@ -1,26 +1,23 @@
 import { propStyles } from "util/style";
-import styles from "./cluster.module.css";
 
 const Cluster = (props) => {
   const { align, children, className = "", justify, space, ...rest } = props;
 
-  const myClass = `${styles.cluster} ${className}`;
-
-  // TODO the global child selector is applying to every cluster on the page. fucked.
+  const myClass = `cluster ${className}`;
 
   return (
     <div className={myClass} {...rest}>
-      <div>
-        {children}
-        <style jsx>{`
-          .${styles.cluster} {
-            ${propStyles([space, "--space"])}
-          }
-          :global(.${styles.cluster} > *) {
-            ${propStyles([justify, "justify-content"], [align, "align-items"])}
-          }
-        `}</style>
-      </div>
+      <div>{children}</div>
+      <style jsx>{`
+        .cluster {
+          ${propStyles([space, "--space"])}
+        }
+      `}</style>
+      <style jsx>{`
+        .cluster > :global(*) {
+          ${propStyles([justify, "justify-content"], [align, "align-items"])}
+        }
+      `}</style>
     </div>
   );
 };
