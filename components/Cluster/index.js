69f2ac6355895e4d7fd762c65470c7747538d43d
andrew stebenne <arthropodSeven@protonmail.com>
2021-03-22 18:43:01 -0400

    fix an issue with the Cluster. im really stupid

--- components/Cluster/index.js
+++ components/Cluster/index.js
@@ -6,21 +6,17 @@ const Cluster = (props) => {
 
   const myClass = `${styles.cluster} ${className}`;
 
-  // TODO the global child selector is applying to every cluster on the page. fucked.
-
   return (
     <div className={myClass} {...rest}>
-      <div>
-        {children}
-        <style jsx>{`
-          .${styles.cluster} {
-            ${propStyles([space, "--space"])}
-          }
-          :global(.${styles.cluster} > *) {
-            ${propStyles([justify, "justify-content"], [align, "align-items"])}
-          }
-        `}</style>
-      </div>
+      <div>{children}</div>
+      <style jsx>{`
+        .${styles.cluster} {
+          ${propStyles([space, "--space"])}
+        }
+        .${styles.cluster} > :global(*) {
+          ${propStyles([justify, "justify-content"], [align, "align-items"])}
+        }
+      `}</style>
     </div>
   );
 };
