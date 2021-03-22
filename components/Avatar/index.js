6fd5f9f520e6f8c710ecc88888f3c48560603ae4
andrew stebenne <arthropodSeven@protonmail.com>
2021-03-22 19:14:24 -0400

    remove the largest version of the avatar image

--- components/Avatar/index.js
+++ components/Avatar/index.js
@@ -4,7 +4,7 @@ const sources = [
   ["/images/me_qtr_115.jpg", "115w"],
   ["/images/me_half_230.jpg", "230w"],
   ["/images/me_three_qtr_345.jpg", "345w"],
-  ["/images/me_full_460.jpg", "460w"],
+  // ["/images/me_full_460.jpg", "460w"],
 ];
 const srcSet = sources.map((pair) => pair.join(" ")).join(", ");
 
@@ -18,7 +18,7 @@ const Avatar = (props) => {
       className={myClass}
       alt="An iridescent golden beetle on a fingertip"
       srcSet={srcSet}
-      sizes="(min-width: 180ch) 460px, (min-width: 160ch) 345w, 230px"
+      sizes="(min-width: 160ch) 345w, 230px"
       src="/images/me_three_qtr_345.jpg"
       {...rest}
     />
