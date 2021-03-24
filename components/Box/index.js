a82d5de698702d0d95c72a4ec9a97796194b6ea9
andrew stebenne <arthropodSeven@protonmail.com>
2021-03-23 20:12:49 -0400

    add prop signature for Box

--- components/Box/index.js
+++ components/Box/index.js
@@ -1,5 +1,12 @@
 import { propStyles } from "util/style";
 
+/**
+ * @typedef {Object} BoxProps
+ * @prop {string=} borderWidth - Box border width
+ * @prop {string=} padding - Box padding (all sides)
+ */
+
+/** @param {BoxProps} props */
 const Box = (props) => {
   const { borderWidth, children, className = "", padding, ...rest } = props;
 
