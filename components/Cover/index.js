46b25916ffefa554790f921642f986b3a7658084
andrew stebenne <arthropodSeven@protonmail.com>
2021-03-23 20:34:41 -0400

    add props signature for Cover

--- components/Cover/index.js
+++ components/Cover/index.js
@@ -1,5 +1,13 @@
 import { propStyles } from "util/style";
 
+/**
+ * @typedef {Object} CoverProps
+ * @prop {string=} minHeight - Minimum height (default 100vh)
+ * @prop {string=} space - Space between elements
+ * @prop {boolean=} noPad - Whether to remove padding around the Cover
+ */
+
+/** @param {CoverProps} props */
 const Cover = (props) => {
   const { children, className = "", minHeight, space, noPad } = props;
 
