bc9e56fc21d62203713802622d8321bd129577ce
andrew stebenne <arthropodSeven@protonmail.com>
2021-03-26 14:24:51 -0400

    fixup! Add prop typedef for Switcher

--- components/Switcher/index.js
+++ components/Switcher/index.js
@@ -1,10 +1,13 @@
 import { propStyles } from "util/style";
 
+/* TODO Add prop: exceptions [[childIdx, flexGrowValue]] - see here
+ * https://every-layout.dev/layouts/switcher/#managing-proportions */
+
 /**
  * @typedef {Object} SwitcherProps
  * @prop {string=} threshold - Below this width, switch to the vertical layout.
  * @prop {string=} space - Total margin between siblings.
- * @prop {number=} limit - With more than `limit` children, use only the vertical layout.
+ * @prop {number=} limit - With more than `limit` children, use only the vertical layout. Default 7.
  */
 
 /** @param {SwitcherProps} props */
@@ -12,8 +15,6 @@ const Switcher = (props) => {
   const {
     children,
     className = "",
-    /* TODO Add prop: exceptions [[childIdx, flexGrowValue]] - see here
-     * https://every-layout.dev/layouts/switcher/#managing-proportions */
     threshold,
     space,
     limit = 7,
