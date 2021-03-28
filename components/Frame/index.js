2d8cbb305a79485fc789b69c4a8afb25331ddf16
andrew stebenne <arthropodSeven@protonmail.com>
2021-03-28 15:40:31 -0400

    First stab at the Frame

--- components/Frame/index.js
+++ components/Frame/index.js
@@ -1,12 +1,31 @@
 import { propStyles } from "util/style";
 
+const separators = /[/:]/;
+const getRatioValues = (raw) => {
+  if (!raw) {
+    return [undefined, undefined];
+  }
+  const ratioSplit = raw.split(separators);
+  if (ratioSplit.length !== 2) {
+    throw new Error(`Ratio ${raw} is invalid for Frame`);
+  }
+  return ratioSplit.map(Number);
+};
+
 const Frame = (props) => {
-  const { children } = props;
+  const { children, ratio: rawRatio } = props;
+
+  const [numerator, denominator] = getRatioValues(rawRatio);
+
   return (
     <div>
       {children}
       <style jsx>{`
         .frame {
+          ${propStyles(
+            [numerator, "--aspect-numerator"],
+            [denominator, "--aspect-denominator"]
+          )}
         }
       `}</style>
     </div>
