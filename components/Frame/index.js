0ef9674022989df523be432b996697e7ed2d716a
andrew stebenne <arthropodSeven@protonmail.com>
2021-03-28 15:45:11 -0400

    Add prop signature and usage description for Frame

--- components/Frame/index.js
+++ components/Frame/index.js
@@ -12,6 +12,15 @@ const getRatioValues = (raw) => {
   return ratioSplit.map(Number);
 };
 
+/**
+ * @typedef {Object} FrameProps
+ * @prop {string=} ratio - Aspect ratio for the frame, e.g.: "4:3", "13/7". Defaults to "16:9".
+ */
+
+/**
+ * Creates a box with a specified aspect ratio, and crops content to fit. Cropping is destructive, so don't use this for critical content.
+ * @param {FrameProps} props
+ */
 const Frame = (props) => {
   const { children, ratio: rawRatio } = props;
 
