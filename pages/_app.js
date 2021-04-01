e08e05df1b5ace3bb2d9fd9bf9f2f9058d53e935
andrew stebenne <arthropodSeven@protonmail.com>
2021-04-01 16:53:47 -0400

    pass page props to _app.js to decide whether to Center the page

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,21 @@ import { PageCenter } from "components/Center";
 import Header from "components/Header";
 import "styles/index.css";
 
-const MyApp = ({ Component, pageProps }) => (
-  <React.Fragment>
-    <Header />
-    <PageCenter>
-      <Component {...pageProps} />
-    </PageCenter>
-  </React.Fragment>
-);
+const MyApp = ({ Component, pageProps: completeProps }) => {
+  const { _app: appProps = {}, ...pageProps } = completeProps;
+  const { center = true } = appProps;
+  return (
+    <React.Fragment>
+      <Header />
+      {center ? (
+        <PageCenter>
+          <Component {...pageProps} />
+        </PageCenter>
+      ) : (
+        <Component {...pageProps} />
+      )}
+    </React.Fragment>
+  );
+};
 
 export default MyApp;
