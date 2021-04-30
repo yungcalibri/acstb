1ca31a77be53af16765681b83d1ea46b593c15ce
andrew stebenne <arthropodSeven@protonmail.com>
2021-04-30 17:02:35 -0400

    add default meta tags and webmention links to all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from "next/head";
 import { PageCenter } from "components/Center";
 import Header from "components/Header";
 import "styles/index.css";
@@ -8,6 +9,26 @@ const MyApp = ({ Component, pageProps: completeProps }) => {
   const { center = true } = appProps;
   return (
     <React.Fragment>
+      <Head>
+        <title>Andrew Stebenné</title>
+        <meta
+          property="og:title"
+          content="acstb.name - Andrew Stebenné's Personal Website"
+          key="title"
+        />
+        <meta name="author" content="Andrew Stebenné" key="author" />
+        <meta
+          name="description"
+          content="I'm a Web developer, and this is my blog. You can get in touch with me here, or find links to my other social media profiles."
+          key="description"
+        />
+        <meta name="color-scheme" content="dark light" />
+        <link
+          rel="webmention"
+          href="https://webmention.io/acstb.name/webmention"
+        />
+        <link rel="pingback" href="https://webmention.io/acstb.name/xmlrpc" />
+      </Head>
       <Header />
       {center ? (
         <PageCenter>
