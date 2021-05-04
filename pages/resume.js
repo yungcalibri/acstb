2cbe001ab6d477e97b9e5a4fef7a25c5d2c76995
andrew stebenne <arthropodSeven@protonmail.com>
2021-05-03 20:40:41 -0400

    add resume page

---
+++ pages/resume.js
@@ -0,0 +1,11 @@
+export default function Resume() {
+  return (
+    <article id="resume" className="h-resume">
+      <h1 className="p-name">Andrew Stebenné's Résumé</h1>
+      <p className="p-summary">
+        I'm a skilled software engineer, and my forté is building websites. I
+        care about my work, and I go the extra mile to do it properly.
+      </p>
+    </article>
+  );
+}
