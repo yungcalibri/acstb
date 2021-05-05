283a27144d266b66d666f94afb9be95b23d81cd7
andrew stebenne <arthropodSeven@protonmail.com>
2021-05-05 19:29:44 -0400

    put the heading in a header and add contact info

--- pages/resume/index.js
+++ pages/resume/index.js
@@ -1,10 +1,14 @@
+import Head from "next/head";
+import Box from "components/Box";
 import Cluster from "components/Cluster";
 import Stack from "components/Stack";
-import Head from "next/head";
+import Icon from "components/Icon";
 import LineItem from "./components/LineItem";
 import Skill from "./components/Skill";
 
 const copy = {
+  name: "Andrew Stebenné",
+  job: "Web Developer",
   title: "Andrew Stebenné - Web Developer",
   summary: "I'm a software engineer, and my forté is building websites.",
 };
@@ -40,136 +44,164 @@ const Resume = () => (
         key="og:url"
       />
     </Head>
-    <h1 className="p-name">{copy.title}</h1>
-    <div className="p-summary">
-      <p>{copy.summary}</p>
-      <p>
-        I consider software engineering to be a craft. I have a thorough
-        understanding of HTML, CSS, and JavaScript, and I can harness them
-        effectively. I go to great pains in building <em>truly responsive</em>{" "}
-        websites which present a unique and consistent voice{"\u2014"}on any
-        device, for any reader.
-      </p>
-    </div>
+    <header>
+      <h1 className="p-name">{copy.name}</h1>
+      <strong className="p-category">{copy.job}</strong>
+    </header>
+    <main>
+      <div className="p-summary">
+        <p>{copy.summary}</p>
+        <p>
+          I consider software engineering to be a craft. I have a thorough
+          understanding of HTML, CSS, and JavaScript, and I can harness them
+          effectively. I go to great pains in building <em>truly responsive</em>{" "}
+          websites which present a unique and consistent voice{"\u2014"}on any
+          device, for any reader.
+        </p>
+      </div>
 
-    <h2>Skills</h2>
-    <Cluster space="var(--s0)">
-      {skills.map(([name, years]) => (
-        <Skill name={name} years={years} key={name} />
-      ))}
-    </Cluster>
+      <h2>Skills</h2>
+      <Cluster space="var(--s0)">
+        {skills.map(([name, years]) => (
+          <Skill name={name} years={years} key={name} />
+        ))}
+      </Cluster>
 
-    <h2>Experience</h2>
-    <Stack space="var(--s1)" asList className="max-width:var(--measure)">
-      <LineItem
-        start="2019"
-        end="2021"
-        url="https://aedit.com/"
-        mainHeading="Senior Software Engineer at AEDIT"
-        location={
-          <div className="p-adr h-adr">
-            <span className="p-locality">New York</span>
-            {", "}
-            <span className="p-region">New York</span>
-            {", "}
-            <span className="p-country-name">United States</span>
-          </div>
-        }>
-        <p>
-          Worked one-on-one with designers and stakeholders to implement new
-          features throughout AEDIT's Web presence, from a new editorial blog
-          platform to a full person-to-person video chat app.
-        </p>
-      </LineItem>
-      <LineItem
-        start="2018"
-        end="2019"
-        url="https://domio.com/"
-        mainHeading="Software Engineer at Domio"
-        location={
-          <div className="p-adr h-adr">
-            <span className="p-locality">New York</span>
-            {", "}
-            <span className="p-region">New York</span>
-            {", "}
-            <span className="p-country-name">United States</span>
-          </div>
-        }>
-        <p>
-          Contributed new features and improvements to Domio's primary hotel
-          booking Web app, and sharpened my understanding of React.js and Web
-          fundamentals.
-        </p>
-      </LineItem>
-      <LineItem
-        className="p-experience"
-        start="2015"
-        end="2018"
-        url="https://xojet.com/"
-        mainHeading="Software Engineer at XOJet"
-        location={
-          <div className="p-adr h-adr">
-            <span className="p-locality">Sacramento</span>
-            {", "}
-            <span className="p-region">California</span>
-            {", "}
-            <span className="p-country-name">United States</span>
-          </div>
-        }>
-        <p>
-          As a member of a small tech team, contributed major features to
-          XOJet's internal Web apps, enabling their booking team to price even
-          the most complex flights faster than ever before.
-        </p>
-      </LineItem>
-    </Stack>
+      <h2>Experience</h2>
+      <Stack space="var(--s1)" asList className="max-width:var(--measure)">
+        <LineItem
+          start="2019"
+          end="2021"
+          url="https://aedit.com/"
+          mainHeading="Senior Software Engineer at AEDIT"
+          location={
+            <div className="p-adr h-adr">
+              <span className="p-locality">New York</span>
+              {", "}
+              <span className="p-region">New York</span>
+              {", "}
+              <span className="p-country-name">United States</span>
+            </div>
+          }>
+          <p>
+            Worked one-on-one with designers and stakeholders to implement new
+            features throughout AEDIT's Web presence, from a new editorial blog
+            platform to a full person-to-person video chat app.
+          </p>
+        </LineItem>
+        <LineItem
+          start="2018"
+          end="2019"
+          url="https://domio.com/"
+          mainHeading="Software Engineer at Domio"
+          location={
+            <div className="p-adr h-adr">
+              <span className="p-locality">New York</span>
+              {", "}
+              <span className="p-region">New York</span>
+              {", "}
+              <span className="p-country-name">United States</span>
+            </div>
+          }>
+          <p>
+            Contributed new features and improvements to Domio's primary hotel
+            booking Web app, and sharpened my understanding of React.js and Web
+            fundamentals.
+          </p>
+        </LineItem>
+        <LineItem
+          className="p-experience"
+          start="2015"
+          end="2018"
+          url="https://xojet.com/"
+          mainHeading="Software Engineer at XOJet"
+          location={
+            <div className="p-adr h-adr">
+              <span className="p-locality">Sacramento</span>
+              {", "}
+              <span className="p-region">California</span>
+              {", "}
+              <span className="p-country-name">United States</span>
+            </div>
+          }>
+          <p>
+            As a member of a small tech team, contributed major features to
+            XOJet's internal Web apps, enabling their booking team to price even
+            the most complex flights faster than ever before.
+          </p>
+        </LineItem>
+      </Stack>
 
-    <h2>Education</h2>
-    <Stack space="var(--s1)" asList className="max-width:var(--measure)">
-      <LineItem
-        className="p-education"
-        start="2012"
-        end="2017"
-        url="https://nau.edu/"
-        mainHeading="Northern Arizona University"
-        location={
-          <div className="p-adr h-adr">
-            <span className="p-locality">Flagstaff</span>,{" "}
-            <span className="p-region">Arizona</span>,{" "}
-            <span className="p-country-name">United States</span>
-          </div>
-        }>
-        <p>
-          Two courses of study:{" "}
-          <span className="p-name">
-            Computer Science and Comparative Cultural Studies
-          </span>
-          .
-        </p>
-      </LineItem>
+      <h2>Education</h2>
+      <Stack space="var(--s1)" asList className="max-width:var(--measure)">
+        <LineItem
+          className="p-education"
+          start="2012"
+          end="2017"
+          url="https://nau.edu/"
+          mainHeading="Northern Arizona University"
+          location={
+            <div className="p-adr h-adr">
+              <span className="p-locality">Flagstaff</span>,{" "}
+              <span className="p-region">Arizona</span>,{" "}
+              <span className="p-country-name">United States</span>
+            </div>
+          }>
+          <p>
+            Two courses of study:{" "}
+            <span className="p-name">
+              Computer Science and Comparative Cultural Studies
+            </span>
+            .
+          </p>
+        </LineItem>
 
-      <LineItem
-        className="p-education"
-        start="2015"
-        end="2016"
-        url="http://english.cqupt.edu.cn/"
-        mainHeading="Chongqing University of Posts and Telecommunications "
-        location={
-          <div className="p-adr h-adr">
-            <span className="p-locality">Nanshan</span>,{" "}
-            <span className="p-region">Chongqing</span>,{" "}
-            <span className="p-country-name">China</span>
-          </div>
-        }>
-        <p>
-          Exchange program:{" "}
-          <span className="p-name">
-            Chinese Language and Culture, and Computer Science
-          </span>
-          .
+        <LineItem
+          className="p-education"
+          start="2015"
+          end="2016"
+          url="http://english.cqupt.edu.cn/"
+          mainHeading="Chongqing University of Posts and Telecommunications "
+          location={
+            <div className="p-adr h-adr">
+              <span className="p-locality">Nanshan</span>,{" "}
+              <span className="p-region">Chongqing</span>,{" "}
+              <span className="p-country-name">China</span>
+            </div>
+          }>
+          <p>
+            Exchange program:{" "}
+            <span className="p-name">
+              Chinese Language and Culture, and Computer Science
+            </span>
+            .
+          </p>
+        </LineItem>
+      </Stack>
+
+      <div className="p-contact h-card">
+        <h2>Contact Me</h2>
+        <p className="p-name display:none">
+          <span className="p-given-name">Andrew</span>
+          <span className="p-family-name"> Stebenné</span>
         </p>
-      </LineItem>
-    </Stack>
+        <p className="p-category display:none">{copy.job}</p>
+        <a className="u-url u-uid display:none" href="https://acstb.name/">
+          https://acstb.name
+        </a>
+        <a href="mailto:arthropodSeven@pm.me">
+          <Icon iconId="envelope">
+            <span className="u-email">arthropodSeven@pm.me</span>
+          </Icon>
+        </a>
+        <p className="p-adr h-adr">
+          <span className="p-locality">Highland Park</span>
+          {", "}
+          <span className="p-region">New Jersey</span>
+          <span className="p-country-name display:none">United States</span>
+        </p>
+      </div>
+    </main>
   </article>
 );
 
