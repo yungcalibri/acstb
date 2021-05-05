c5a95ac8bddb2286bf624afbc9fec0e1d99a6948
andrew stebenne <arthropodSeven@protonmail.com>
2021-05-05 17:54:29 -0400

    add dashed line between dates in line items

--- pages/resume/components/LineItem.js
+++ pages/resume/components/LineItem.js
@@ -14,25 +14,25 @@ const LineItem = (props) => {
   } = props;
 
   return (
-    <Box
-      borderWidth="var(--s-4)"
-      padding="0"
-      className={`${className} line-item h-event display:flex`}>
-      <div className="invert-title" style={{ padding: "var(--s-1)" }}>
-        <strong>
-          <time className="dt-start" dateTime={start}>
-            {startDisplay || start}
-          </time>
-          {" - "}
-          <time className="dt-end" dateTime={end}>
-            {endDisplay || end}
-          </time>
-        </strong>
+    <div className={`${className} line-item h-event display:flex`}>
+      <div className="invert-title">
+        <small>
+          <strong>
+            <time className="dt-end" dateTime={end}>
+              {endDisplay || end}
+            </time>
+          </strong>
+        </small>
+        <b />
+        <small>
+          <strong>
+            <time className="dt-start" dateTime={start}>
+              {startDisplay || start}
+            </time>
+          </strong>
+        </small>
       </div>
-      <div
-        style={{
-          padding: "var(--s-1)",
-        }}>
+      <Box padding="var(--s-1)" borderWidth="var(--s-4)">
         <div className="p-location h-card">
           <a className="u-url p-name" href={url}>
             <strong>{mainHeading}</strong>
@@ -40,8 +40,8 @@ const LineItem = (props) => {
           <small>{location}</small>
         </div>
         <p>{children}</p>
-      </div>
-    </Box>
+      </Box>
+    </div>
   );
 };
 
