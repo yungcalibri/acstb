7ffdd64287ef8666df28653afdfa5cb835c955c2
andrew stebenne <arthropodSeven@protonmail.com>
2021-05-20 19:10:15 -0400

    add a new validation todo

--- components/Cover/index.js
+++ components/Cover/index.js
@@ -11,6 +11,10 @@ import { propStyles } from "util/style";
  * - Centers an element vertically.
  * - When there are two or three children, the central element must have the class `centered`. The first child will appear at the top of the space, and the last will appear at the bottom.
  * @todo Apply centering to `:only-child`.
+ * @todo Validate that one or both of these conditions is true:
+ *  1. The Cover has exactly one child
+ *  2. The Cover has exactly two children, AND one of them has the "centered" class
+ *  3. The Cover has exactly three children, AND the second child has the "centered" class
  * @param {CoverProps} props
  * */
 const Cover = (props) => {
