e39b99aebaa10497e8ea1be977eb70e30fba1d45
andrew stebenne <arthropodSeven@protonmail.com>
2021-05-20 18:57:42 -0400

    fixup! add styled 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,12 @@
 import Cover from "components/Cover";
-import Center from "components/Center";
 
 export default function Error404() {
   return (
     <Cover minHeight="80vh">
-      <Center className="centered">
+      <div className="centered">
         <h1>Error 404</h1>
         <p>The page you're looking for doesn't exist.</p>
-      </Center>
+      </div>
     </Cover>
   );
 }
