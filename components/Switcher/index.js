1db3569ccd9f6fecc982f44c18e3bcce15dec8b2
andrew stebenne <arthropodSeven@protonmail.com>
2021-05-29 19:42:00 -0400

    todo done: add exceptions prop to Switcher
    note- validation only, not implementation

--- components/Switcher/index.js
+++ components/Switcher/index.js
@@ -1,8 +1,5 @@
 import { propStyles } from "util/style";
 
-/* TODO Add prop: exceptions [[childIdx, flexGrowValue]] - see here
- * https://every-layout.dev/layouts/switcher/#managing-proportions */
-
 /**
  * @typedef {Object} SwitcherProps
  * @prop {string=} threshold - Below this width, switch to the vertical layout.
@@ -13,16 +10,20 @@ import { propStyles } from "util/style";
 /**
  * Displays children horizontally with equal length, when space permits. When space is insufficient, or children too numerous, switches to a vertical layout.
  * @param {SwitcherProps} props
+ * @todo Add style features for prop `exceptions` - see https://every-layout.dev/layouts/switcher/#managing-proportions
  */
 const Switcher = (props) => {
   const {
     children,
     className = "",
+    exceptions = [],
     threshold,
     space,
     limit = 7,
     ...rest
   } = props;
+
+  validateExceptions(exceptions);
 
   const myClass = `switcher ${className}`;
 
@@ -45,3 +46,51 @@ const Switcher = (props) => {
 };
 
 export default Switcher;
+
+const validateExceptions = (exs) => {
+  if (!(exs instanceof Array)) {
+    throw new Error(
+      `Switcher error: the value of \`exceptions\` must be an array`
+    );
+  }
+  if (exs.length < 1) {
+    return;
+  }
+  exs.forEach((exEntry, idx) => {
+    if (!(exEntry instanceof Array) || exEntry.length !== 2) {
+      throw new Error(
+        `Switcher error: exception ${idx} (${exEntry}) should be a two-entry array`
+      );
+    }
+  });
+  exs.forEach(([ex, flexGrow], idx) => {
+    if (typeof ex !== "number") {
+      throw new Error(
+        `Switcher error: child index ${ex} in exception ${
+          idx + 1
+        } ([${ex}, ${flexGrow}]) must be a number`
+      );
+    }
+    if (ex < 0) {
+      throw new Error(
+        `Switcher error: child index ${ex} in exception ${
+          idx + 1
+        } ([${ex}, ${flexGrow}]) must be greater than zero`
+      );
+    }
+    if (typeof flexGrow !== "number") {
+      throw new Error(
+        `Switcher error: flex-grow value ${flexGrow} in exception ${
+          idx + 1
+        } ([${ex}, ${flexGrow}]) must be a number`
+      );
+    }
+    if (flexGrow < 1) {
+      throw new Error(
+        `Switcher error: child index ${flexGrow} in exception ${
+          idx + 1
+        } ([${ex}, ${flexGrow}]) must be greater than one`
+      );
+    }
+  });
+};
