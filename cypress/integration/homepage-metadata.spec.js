015d9d5868aef572039cc5dd85badad49a4e216c
andrew stebenne <arthropodSeven@protonmail.com>
2021-06-09 16:39:53 -0400

    test- validate homepage metadata

---
+++ cypress/integration/homepage-metadata.spec.js
@@ -0,0 +1,61 @@
+context("Validate Metadata", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  context("meta tags", () => {
+    it("has the standard meta tags and content", () => {
+      cy.get("meta[charset]").should("have.attr", "charset").and("eq", "utf-8");
+
+      cy.get("meta[name=author]")
+        .should("have.attr", "content")
+        .and("eq", "Andrew Stebenné");
+
+      cy.get("meta[name=description]")
+        .should("have.attr", "content")
+        .and("not.be.empty");
+    });
+
+    it("has OpenGraph meta tags and content", () => {
+      cy.get("meta[property='og:title']")
+        .should("have.attr", "content")
+        .and("not.be.empty");
+
+      cy.get("meta[property='og:image']")
+        .should("have.attr", "content")
+        .and("not.be.empty");
+
+      cy.get("meta[property='og:type']")
+        .should("have.attr", "content")
+        .and("eq", "website");
+
+      cy.get("meta[property='og:site_name']")
+        .should("have.attr", "content")
+        .and("eq", "acstb.name");
+    });
+  });
+
+  context("link tags", () => {
+    it("has a manifest.json", () => {
+      cy.get("link[rel=manifest]")
+        .should("have.attr", "href")
+        .and("include", "manifest.json");
+    });
+
+    it("has favicons", () => {
+      cy.get("link[rel=icon]")
+        .should("have.attr", "href")
+        .and("include", "favicon.ico");
+
+      cy.get("link[rel='apple-touch-icon-precomposed']")
+        .should("have.attr", "href")
+        .and("include", "apple-touch-icon.png");
+    });
+
+    it("has a webmention backlink", () => {
+      cy.get("link[rel=webmention]")
+        .should("have.attr", "href")
+        .and("include", "acstb.name");
+    });
+  });
+});
