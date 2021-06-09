b1d165cc70e052f1af67f73600cac2e626480b07
andrew stebenne <arthropodSeven@protonmail.com>
2021-06-09 17:42:37 -0400

    flesh out the resume spec some more

--- cypress/integration/resume.spec.js
+++ cypress/integration/resume.spec.js
@@ -18,6 +18,11 @@ context("Resume", () => {
       cy.get("article").should("have.attr", "id").and("eq", "resume");
     });
 
+    it("has a single <main> as a child of the <article>", () => {
+      cy.get("main").should("have.length", 1);
+      cy.get("article main").should("have.length", 1);
+    });
+
     it("has only one <h1>", () => {
       cy.get("#__next").debug();
       cy.get("h1");
@@ -27,6 +32,34 @@ context("Resume", () => {
       cy.get("a[href^='mailto']")
         .should("have.attr", "href")
         .and("include", "arthropodSeven@pm.me");
+    });
+
+    it("includes a list of skills", () => {
+      cy.get("h2")
+        .contains("Skills")
+        .next()
+        .should("be.visible")
+        .find(".skill")
+        .should("have.length.gte", 1);
+    });
+
+    it("includes my work history", () => {
+      cy.get("h2")
+        .contains("Experience")
+        .next()
+        .should("be.visible")
+        .children()
+        .should("have.class", "h-event");
+    });
+
+    it("includes my education history", () => {
+      cy.get("h2")
+        .contains("Education")
+        .next()
+        .should("be.visible")
+        .children()
+        .should("have.class", "p-education")
+        .and("have.class", "h-event");
     });
   });
 });
