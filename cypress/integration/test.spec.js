07e67aef18653055d1337464dc9b429b7cd32816
andrew stebenne <arthropodSeven@protonmail.com>
2021-06-09 12:54:19 -0400

    simpler test

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -3,10 +3,7 @@ context("Test Spec", () => {
     cy.visit("http://localhost:3000");
   });
 
-  it("the nav contains a home link", () => {
-    cy.get("nav#root-nav").find("a#home-link");
-  });
-  it("the home link should point to the root", () => {
+  it("the nav contains a home link which points to the root", () => {
     cy.get("nav#root-nav")
       .find("a#home-link")
       .should("have.attr", "href")
