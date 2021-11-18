b68f97fbc204349a04091c7d3d9d68911a6cc392
andrew stebenne <arthropodSeven@protonmail.com>
2021-11-18 14:44:41 -0500

    Simplify cluster, remove element prop

--- components/Cluster/index.js
+++ components/Cluster/index.js
@@ -1,15 +1,11 @@
-import { Children, memo, useEffect } from "react";
+import { Children, useEffect } from "react";
 import { propStyles } from "util/style";
-
-const MemoEl = ({ Element = "div", ...rest }) => <Element {...rest} />;
-const El = memo(MemoEl);
 
 /**
  * @typedef {Object} ClusterProps
  * @prop {string=} align - Value of `align-items`
  * @prop {string=} justify - Value of `justify-content`
  * @prop {string=} space - Padding between items in cluster
- * @prop {boolean=} asList - Mark up the cluster as a list
  */
 
 /**
@@ -21,7 +17,6 @@ const Cluster = (props) => {
     align,
     children,
     className = "",
-    element = "div",
     justify,
     space,
     role,
@@ -38,7 +33,7 @@ const Cluster = (props) => {
   const myClass = `cluster ${className}`;
 
   return (
-    <El Element={element} className={myClass} role={role} {...rest}>
+    <div className={myClass} role={role} {...rest}>
       {children}
       <style jsx>{`
         .cluster {
@@ -49,7 +44,7 @@ const Cluster = (props) => {
           )}
         }
       `}</style>
-    </El>
+    </div>
   );
 };
 
