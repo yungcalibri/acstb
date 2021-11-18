1e5b7f224597b5cfb0a4755a25b5c95de4a2468b
andrew stebenne <arthropodSeven@protonmail.com>
2021-11-18 16:52:02 -0500

    update header

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,18 +1,25 @@
 import Link from "next/link";
 import { PageCenter } from "components/Center";
+import Cluster from "components/Cluster";
 import Icon from "components/Icon";
 
 const Header = () => (
   <header>
     <PageCenter>
       <nav id="root-nav">
-        <Link href="/">
-          <a id="home-link">
-            <Icon iconId="home" head lower>
-              acstb.name
-            </Icon>
-          </a>
-        </Link>
+        <Cluster role="list">
+          {/** @todo fix duplicated role=listitem here, conflict with Cluster validation */}
+          <Link href="/" role="listitem">
+            <a id="home-link" role="listitem">
+              <Icon iconId="home" head>
+                acstb.name
+              </Icon>
+            </a>
+          </Link>
+          <Link href="/resume" role="listitem">
+            <a role="listitem">Resume</a>
+          </Link>
+        </Cluster>
       </nav>
       {/* Should swap out the self-aggrandizement for some breadcrumbs here when the time comes. */}
     </PageCenter>
